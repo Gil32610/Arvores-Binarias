240e5f321e1b16bb439891a36a0a7054773d6122
GIL32610 <gil.00000032610@unicap.br>
2022-11-22 10:43:28 -0300

    recursao

--- src/br/unicap/edi/trees/BinaryTree.java
+++ src/br/unicap/edi/trees/BinaryTree.java
@@ -11,12 +11,11 @@ public class BinaryTree<T extends Comparable<T>> {
         }
     }
 
-    public void insert(T content){
+    public void insert(T content) {
         Node<T> aNode = new Node<>(content);
-        if(this.isEmpty()){
+        if (this.isEmpty()) {
             this.root = aNode;
-        }
-        else{
+        } else {
             this.root.insertNode(content);
         }
     }
@@ -27,6 +26,22 @@ public class BinaryTree<T extends Comparable<T>> {
 
     public void setRoot(Node<T> root) {
         this.root = root;
+    }
+
+    public void inOrder(){
+        if(this.isEmpty()){
+            System.out.println("Arvore vazia");
+        }else{
+            percorrerEmOrdem(this.root)
+        }
+    }
+
+    private void percorrerEmOrdem(Node<T> root2) {
+        if (root2 != null) {
+            percorrerEmOrdem(root2.getLeft());
+            System.out.println(root2.getContent());
+            percorrerEmOrdem(root2.getRight());
+        }
     }
 
 }
